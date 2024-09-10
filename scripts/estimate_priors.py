02dfb25228ab45bf5702c4250a56b49ff977e9e4
Alexia Mahlig <alexia.mahlig@polymtl.ca>
2024-09-10 15:01:01 -0400

    Changed estimate_priors to only T2 with london priors

--- scripts/estimate_priors.py
+++ scripts/estimate_priors.py
@@ -7,18 +7,18 @@ path_synthsr = '/home/ge.polymtl.ca/almahj/SynthSR/'
 sys.path.append(path_synthsr)
 from SynthSR.estimate_priors import build_intensity_stats
 
-images_dir = [path_synthsr + 'data/images/T1/', path_synthsr + '/data/images/T2/']
-labels_dir = path_synthsr + 'data/labels/labels/'
+images_dir = [path_synthsr + 'data/images/HYP/']
+labels_dir = [path_synthsr + 'data/labels/HYP/']
 
-label_files = [os.path.join(labels_dir, f) for f in os.listdir(labels_dir) if f.endswith('.mgz')]
-print(label_files)
+#os.system(f'flirt -in {labels_dir}london_labels.nii -ref {images_dir}london.nii -out {labels_dir}london_labels.nii -omat transformation.mat')
+#os.system(f'flirt -in {labels_dir}london_labels.nii -ref {images_dir}london.nii -applyxfm -init transformation.mat -out {labels_dir}london_labels.nii')
+#os.remove('transformation.mat')
 
 # Numpy arrays with the labels corresponding to samseg
 estimation_labels = np.array([0, 24, 16, 4, 14, 15, 2, 12, 85, 3, 7, 8, 10, 11, 13, 17, 18, 26, 28, 42, 43, 46, 47, 49, 50, 52, 53, 54, 58, 60]) 
 estimation_classes = np.array([0, 1, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 7, 3, 8, 9, 10, 11, 12, 13, 14, 15, 16]) # Classes grouping left/right labels and the ventricules
 
-
-result_dir = path_synthsr + 'images/labels_classes_priors/basel_priors/'
+result_dir = path_synthsr + 'images/labels_classes_priors/london_priors/'
 
 build_intensity_stats(list_image_dir=images_dir,
                       list_labels_dir=labels_dir,
